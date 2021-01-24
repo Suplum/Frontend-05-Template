408ab662f2fb58a3eed2986ae48e5425fe24461b
Suplum <799819167@qq.com>
2021-01-24 16:12:08 +0800

    实现flick事件

--- Week 15/GESTURE/gesture.js
+++ Week 15/GESTURE/gesture.js
@@ -101,6 +101,11 @@ let isPan = false, isTap = true, isPress = false;
 let start = (point, context) => {
   // console.log("start", point.clientX, point.clientY)
   context.startX = point.clientX, context.startY = point.clientY;
+  context.points = [{
+    t: Date.now(),
+    x: point.clientX,
+    y: point.clientY
+  }];
 
   context.isTap = false;
   context.isPan = false;
@@ -131,6 +136,12 @@ let move = (point, context) => {
     console.log("pan");
   }
 
+  context.points = context.points.filter(point => Date.now() - point.t < 500)
+  context.points.push({
+    t: Date.now(),
+    x: point.clientX,
+    y: point.clientY
+  })
   // console.log("move", point.clientX, point.clientY)
 }
 
@@ -146,6 +157,25 @@ let end = (point, context) => {
   if(context.isPress) {
     console.log("pressend")
   }
+  context.points = context.points.filter(point => Date.now() - point.t < 500)
+
+  let d, v;
+  if(!context.points.length) {
+    v = 0;
+  } else {
+    d = Math.sqrt((point.clientX - context.points[0].x) ** 2 + 
+      (point.clientY - context.points[0].y) ** 2);
+    v = d / (Date.now() - context.points[0].t)
+  }
+
+  if(v > 1.5) {
+    console.log("flick");
+    context.isFlick = true;
+  } else {
+    context.isFlick = false;
+  }
+  
+  console.log(v)
   // console.log("end", point.clientX, point.clientY)
 }
 
